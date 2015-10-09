cd049619152ab9822e4d7d7a32b93eaf227c96b8
Daniel Genser <daniel.genser@gmail.com>
2015-10-09 03:32:07 +0200

    Updating FB link preview img

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
 	<link rel="shortcut icon" type="image/x-icon" href="<?php bloginfo('template_url'); ?>/img/favicon.png">
 	<link href='https://fonts.googleapis.com/css?family=Fira+Mono:400,700' rel='stylesheet' type='text/css'> 
 	<!-- Facebook share default img -->
+	<meta property="og:type" content="article" />
 	<meta property="og:image" content="<?php bloginfo('template_url'); ?>/img/social-media-default-share.png" />
 	<?php wp_head(); ?>
 </head>
